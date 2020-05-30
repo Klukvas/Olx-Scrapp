eeae66db44cc3d3ad940911980b402f0863dadb8
Klukvas <programmer56457@gmail.com>
2020-05-31 01:16:22 +0300

    fixed bug with getting phone & same log messages multiple times

--- olx_scrapp.py
+++ olx_scrapp.py
@@ -7,8 +7,8 @@ from models import DataBaseClient, ModelRecord
 
 from settings import path_to_driver, start_url
 from Logging import Logger
-
-from  re import match
+from  re import search
+from selenium.common import exceptions
 
 class ScrappOlx:
     def __init__(self):
@@ -46,21 +46,68 @@ class ScrappOlx:
             for number in range(1, int(self.max_page)+1):
                 if not self.first_page:
                     self.new_url = item+f'?page={number}'
-                    print(f'self.new_url: {self.new_url}')
                     self.driver.get(self.new_url)
                 all_records_on_page = self.driver.find_elements(
-                    By.XPATH, '//a[contains(@class, "detailsLink")]'
+                    By.XPATH, '//tr[contains(@class, "wrap")]//a[contains(@class, "linkWithHash detailsLink")]'
                     )
                 href_to_records = [
                     item.get_attribute('href') for item in all_records_on_page
                 ]
                 self.first_page = False
+                href_to_records = list(set(href_to_records))
                 self.get_info_record(href_to_records)
+
+    def get_phone_number(self):
+        try:
+            self.driver.find_element(
+                By.XPATH, '//div[contains(@id, "cookiesBar")]/button[contains(@class, "cookiesBarClose")]'
+            ).click()
+        except Exception as err:
+            pass
+        try:
+            self.wait.until(EC.element_to_be_clickable((
+                By.XPATH, '//div[contains(@class, "contact-button link-phone")]/strong[contains(@class, "xx-large")]'
+            ))).click()
+            phone = self.driver.find_element(
+                By.XPATH, '//div[contains(@class, "contact-button link-phone")]/strong[contains(@class, "xx-large")]'
+                ).text
+            test = search(r'\d+', phone)
+        except (exceptions.TimeoutException, exceptions.NoSuchElementException) as no_element:
+            test = 1
+            self.log.warning(f'No phone on record: {self.driver.current_url}')
+            phone = ' '
+        except exceptions.StaleElementReferenceException as err:
+            self.driver.refresh()
+            try:
+                self.wait.until(EC.element_to_be_clickable((
+                    By.XPATH, '//div[contains(@class, "contact-button link-phone")]/strong[contains(@class, "xx-large")]'
+                ))).click()
+                phone = self.driver.find_element(
+                    By.XPATH, '//div[contains(@class, "contact-button link-phone")]/strong[contains(@class, "xx-large")]'
+                    ).text
+                test = search(r'\d+', phone)
+            except (exceptions.TimeoutException, exceptions.NoSuchElementException) as no_element:
+                test = 1
+                self.log.warning(f'No phone on record: {self.driver.current_url}')
+                phone = ' '
+        while not test:
+            if phone == 'Показать телефон':
+                self.wait.until(EC.element_to_be_clickable((
+                    By.XPATH, '//div[contains(@class, "contact-button link-phone")]/strong[contains(@class, "xx-large")]'
+                ))).click()
+            phone = self.driver.find_element(
+                By.XPATH, '//strong[contains(@class, "xx-large")]'
+            ).text
+            test = search(r'\d+', phone)
+        return phone
 
     def get_info_record(self, hrefs):
         for item in hrefs:
             self.log.info(f'Start parse record\n{item}')
             self.driver.get(item)
+            self.wait.until(EC.element_to_be_clickable((
+                By.XPATH, '//span[contains(@class, "link inlblk")]'
+            )))
             try:
                 no_active = driver.find_element(
                     By.XPATH, '//h3/strong'
@@ -97,31 +144,7 @@ class ScrappOlx:
                 name_user = self.driver.find_element(
                     By.XPATH, '//div[contains(@class, "offer-user__actions")]/h4'
                     ).text
-
-                try:
-                    self.driver.find_element(
-                        By.XPATH, '//div[contains(@id, "cookiesBar")]/button[contains(@class, "cookiesBarClose")]'
-                    ).click()
-                except Exception as err:
-                    self.log.warning(f'{err.args}')
-                try:
-                    self.wait.until(EC.element_to_be_clickable((
-                        By.XPATH, '//div[contains(@class, "contact-button link-phone")]'
-                    ))).click()
-                    phone = self.driver.find_element(
-                        By.XPATH, '//strong[contains(@class, "xx-large")]'
-                        ).text
-                    test = match(r'\d+', phone)
-                    if not test:
-                        self.wait.until(EC.element_to_be_clickable((
-                        By.XPATH, '//div[contains(@class, "contact-button link-phone")]'
-                        ))).click()
-                        phone = self.driver.find_element(
-                            By.XPATH, '//strong[contains(@class, "xx-large")]'
-                        ).text
-                except Exception as err:
-                    phone=''
-                    self.log.warning(f'Can not getting phone: {err.args}')
+                phone = self.get_phone_number()
                 try:
                     image_href = self.driver.find_element(
                         By.XPATH, '//div[contains(@id, "descImage")]/img'
@@ -148,4 +171,7 @@ class ScrappOlx:
                     self.log.info(f'Record {number_record} added to DB')
                 except Exception as err:
                     self.log.error('Record {number_record} nont added to DB {err.args}')
+    def __del__(self):
+        self.driver.clsoe()
+        self.log.info('Scrapping end')
 t = ScrappOlx().get_info_category()
